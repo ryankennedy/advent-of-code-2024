4c786a7055240d30e57ba6c570496afccf4d0740
Ryan Kennedy <rckenned@gmail.com>
2024-12-03 13:40:12 -0800

    Starting day02

---
+++ day02/puzzle01.go
@@ -0,0 +1,7 @@
+package day02
+
+import "fmt"
+
+func Puzzle01() (int, error) {
+	return -1, fmt.Errorf("not implemented")
+}
--- main.go
+++ main.go
@@ -4,18 +4,29 @@ import (
 	"log"
 
 	"github.com/ryankennedy/advent-of-code-2024/day01"
+	"github.com/ryankennedy/advent-of-code-2024/day02"
 )
 
+type PuzzleFunc func() (int, error)
+
+type Puzzle struct {
+	Day    int
+	Puzzle int
+	Func   PuzzleFunc
+}
+
 func main() {
-	if result, err := day01.Puzzle01(); err != nil {
-		log.Fatalf("day01 puzzle01 failed: %s", err)
-	} else {
-		log.Println("day01 puzzle01 =", result)
+	puzzles := []Puzzle{
+		Puzzle{1, 1, day01.Puzzle01},
+		Puzzle{1, 2, day01.Puzzle02},
+		Puzzle{2, 1, day02.Puzzle01},
 	}
 
-	if result, err := day01.Puzzle02(); err != nil {
-		log.Fatalf("day01 puzzle02 failed: %s", err)
-	} else {
-		log.Println("day01 puzzle02 =", result)
+	for _, puzzle := range puzzles {
+		result, err := puzzle.Func()
+		if err != nil {
+			log.Fatalf("Day %d Puzzle %d: error %s", puzzle.Day, puzzle.Puzzle, err)
+		}
+		log.Printf("Day %d Puzzle %d: result=%d\n", puzzle.Day, puzzle.Puzzle, result)
 	}
 }
